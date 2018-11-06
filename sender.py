a425aea3d67ffe076c550598a4a74e7c0840c1b8
None <None>
2018-11-05 16:28:30 -0800

    fixed sender only sending ack0, added trace prints

--- sender.py
+++ sender.py
@@ -14,17 +14,15 @@ except socket.error:
 host = 'localhost'
 port = 2163
 
-s.settimeout(3)
-N: int = input('Enter Window Size: ')
+N = input('Enter Window Size: ')
 base = 0
 nextSeq = 0
 startTime = time.time()
 
+testLoss = True
 # Send 7 test messages
 
 while 1:
-    if nextSeq > 11:
-        break
     if nextSeq < base + N:
         try:
             # Send good package
@@ -33,6 +31,7 @@ while 1:
             s.sendto(ip_checksum(msg) + str(nextSeq) + msg, (host, port))
             if base == nextSeq:
                 startTime = time.time()
+            nextSeq += 1
         except socket.error as msg:
             print('Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
             sys.exit()
@@ -46,12 +45,18 @@ while 1:
 
     try:
         # receive data from client (data, addr)
-        print('send: GETTING ACK')
         reply, addr = s.recvfrom(1024)
         ack = reply[0]
-        base = ack + 1
+        print('Got ACK: ' + ack)
+        base = int(ack) + 1
         # if base == nextSeq:
 
     except socket.error as msg:
         print('Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
         sys.exit()
+    if base > 50:
+        break
+
+s.close()
+sys.exit()
+
